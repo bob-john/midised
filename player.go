23f9e66ffc510f032e4c45031aa411010d24b968
Jérôme Bernard <you@example.com>
2022-05-15 18:52:22 +0200

    improve play()

--- player.go
+++ player.go
@@ -8,14 +8,14 @@ import (
 
 func Play(ls EventList) {
 	var j int
-	var t time.Duration
 	var dt = time.Minute / (300 * 96)
 	var ticker = time.NewTicker(dt)
+	var t0 = time.Now()
 	for range ticker.C {
 		if j >= len(ls) {
 			return
 		}
-		t += dt
+		var t = time.Since(t0)
 		var i int
 		for i = j; i < len(ls); i++ {
 			var e = ls[i]
